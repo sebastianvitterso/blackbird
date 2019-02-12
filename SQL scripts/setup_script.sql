94e409e76c3ec0b483b0c1a08ef5d53dcf43a58c
Sebastian <sebastvi@stud.ntnu.no>
2019-02-12 19:53:41 +0100

    Modified .gitignore, added SQL_script folder

---
+++ SQL scripts/setup_script.sql
@@ -0,0 +1,15 @@
+create table user(
+	userID 		int not null,
+    name		varchar(40)
+);
+
+
+INSERT INTO user values(1, "Sebastian");
+INSERT INTO user values(2, "Patrik");
+INSERT INTO user values(3, "Beatrice");
+INSERT INTO user values(4, "Eivind");
+INSERT INTO user values(5, "Francis");
+INSERT INTO user values(6, "Eric");
+
+
+select * from user
