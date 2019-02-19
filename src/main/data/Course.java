364a1b72549276ba950c915a3f9fb5fb51e8f5c9
Patrik <patrikkj@stud.ntnu.no>
2019-02-19 19:37:15 +0100

    Add new fields to 'Course'.

--- src/main/data/Course.java
+++ src/main/data/Course.java
@@ -2,23 +2,39 @@ package main.data;
 
 public class Course {
 	private final String courseCode;
-	private final String name;
-	public Course(String courseCode, String name) {
+	private String name;
+	private String description;
+	
+	
+	public Course(String courseCode, String name, String description) {
 		this.courseCode = courseCode;
 		this.name = name;
+		this.description = description;
 	}
+	
+	
+	public void updateCourse(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+	
 	public String getCourseCode() {
 		return courseCode;
 	}
+	
 	public String getName() {
 		return name;
+	}
+	
+	public String getDescription() {
+		return description;
 	}
 	
 	@Override
 	public String toString() {
 		return getCourseCode();
 	}
-	
+
 	public enum Role {
 		PROFESSOR, ASSISTANT, STUDENT;
 	}
