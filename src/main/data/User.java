b1b16e3c20b1a4e91470f2f290589fe0f30de885
Patrik <patrikkj@stud.ntnu.no>
2019-02-19 19:26:38 +0100

    Add new fields to User class.

--- src/main/data/User.java
+++ src/main/data/User.java
@@ -2,25 +2,65 @@ package main.data;
 
 public class User {
 	private final String username;
-	private final String password;
-	private final String name;
-	public User(String username, String password, String name) {
+	private String password;
+	private String firstName, lastName;
+	private String email;
+	
+	
+	public User(String username, String password, String firstName, String lastName, String email) {
 		this.username = username;
 		this.password = password;
-		this.name = name;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
 	}
+	
+	
 	public String getUsername() {
 		return username;
 	}
+	
 	public String getPassword() {
 		return password;
 	}
+	
 	public String getName() {
-		return name;
+		return String.format("%s %s", firstName, lastName);
 	}
 	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
 	@Override
 	public String toString() {
-		return name;
+		return String.format("User [username=%s, password=%s, firstName=%s, lastName=%s, email=%s]", username, password,
+				firstName, lastName, email);
 	}
+	
 }
