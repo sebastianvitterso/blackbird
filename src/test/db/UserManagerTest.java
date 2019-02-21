c4a93e51809b727afec613431b1a1d4b69fd83d9
Sebastian <sebastvi@stud.ntnu.no>
2019-02-21 13:07:34 +0100

    Fix (statify) LoginManager for tests

--- src/test/db/UserManagerTest.java
+++ src/test/db/UserManagerTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import org.junit.Test;
 
-import main.data.User;
+import main.models.User;
 import main.db.UserManager;
 
 public class UserManagerTest {
@@ -15,22 +15,22 @@ public class UserManagerTest {
 	@Test
 	public void getUserTest() {
 		User result = UserManager.getUser("seb");
-		User seb = new User("seb", "password", "Sebastian Vittersø");
+		User seb = new User("seb", "password", "Sebastian",  "Vittersø", "seb@gruppe58.no");
 		assertEquals(seb, result);
 	}
 	
 	@Test 
 	public void addUserTest() {
 		assertNull(UserManager.getUser("heot"));
-		UserManager.addUser("heot", "password", "Herman Ottsersen");
+		UserManager.addUser("heot", "password", "Herman",  "Ottsersen", "ho@mail.no");
 		assertNotNull(UserManager.getUser("heot"));
 	}
 
 	@Test
 	public void getUsersTest() {
-		User anhe = new User("anhe", "password", "Anil Henry");
-		User lihe = new User("lihe", "password", "Liam Hernandez");
-		User laco = new User("laco", "password", "Latoya Collins");
+		User anhe = new User("anhe", "password", "Anil", "Henry", "anhe@mail.com");
+		User lihe = new User("lihe", "password", "Liam",  "Hernandez", "lihe@mail.com");
+		User laco = new User("laco", "password", "Latoya", "Collins", "laco@mail.com");
 		List<User> result = UserManager.getUsers();
 		List<User> inDatabase = Arrays.asList(anhe, lihe, laco);
 		assertEquals(inDatabase, result);
@@ -39,7 +39,7 @@ public class UserManagerTest {
 	@Test
 	public void deleteUserTest() {
 		User a = UserManager.getUser("seb");
-		User seb = new User("seb", "password", "Sebastian Vittersø");
+		User seb = new User("seb", "password", "Sebastian",  "Vittersø", "seb@gruppe58.no");
 		assertEquals(seb, a); 
 		UserManager.deleteUser("seb");
 		assertNull(UserManager.getUser("seb"));
